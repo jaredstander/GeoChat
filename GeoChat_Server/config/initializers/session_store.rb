5a885d318f00c1db2530d22aed3ebb6fff5cd56a
Dan Hossom <dan@pivotal-guest-449.sf.pivotallabs.com>
2013-09-29 12:22:34 -0700

    add Rails server project directory to Android project repository for tracking

---
+++ GeoChat_Server/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GeoChatServer::Application.config.session_store :cookie_store, key: '_GeoChat_Server_session'
---
+++ GeoChat_Server/test/controllers/secret_messages_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SecretMessagesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
